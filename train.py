add2da45e969eb941e5e09e90d472ec04c09949a
arazthexd <ghanbarpour.araz@gmail.com>
2023-10-17 12:20:06 +0330

    wgcna prep

--- train.py
+++ train.py
@@ -1,41 +1,6 @@
 from data_utils import Sm2Smiles, Smiles2Mol, Mol2Morgan
 import pandas as pd
 
-def runTraining(args, model, optimizer, loss_fn, train_loader, val_loader, transforms_x, transforms_y): # TODO: Dataloader code
-    
-    lr = args['lr']
-    batch_size = args['batch_size']
-    epochs = args['epochs']
-
-
-    for epoch in range(epochs):
-
-        model.train()
-        for data_batch in train_loader:
-            model.zero_grad()
-            optimizer.zero_grad()
-            x_batch = data_batch.copy()
-            y_batch = data_batch.copy()
-            for transform in transforms_x:
-                x_batch = transform(x_batch)
-            for transform in transforms_y:
-                y_batch = transform(y_batch)
-            
-            y_pred = model(x_batch)
-
-            loss = loss_fn(y_pred, y_batch)
-
-            loss.backwards()
-            optimizer.step()
-        
-        
-        model.eval()
-
-
-
-
-
-
 def train(train_loader, val_loader, model, args, loss_fn, optimizer):
     
     lr = args['lr']
